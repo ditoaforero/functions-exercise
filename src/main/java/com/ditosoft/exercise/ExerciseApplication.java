ccbe038a11bf2a957842a7f8be7c155fb841d911
eforero-stratio <eforero@stratio.com>
2020-05-22 10:33:32 -0500

    Carga inicial

---
+++ src/main/java/com/ditosoft/exercise/ExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.ditosoft.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseApplication.class, args);
+	}
+
+}
